a3cb5bccf857f780426904dd51e75f50706e6f4c
lesny.mk@gmail.com <lesny.mk@gmail.com>
2017-07-23 23:45:27 +0200

    add registration for user

--- src/main/java/com/gameshop/dao/ProductDAOImpl.java
+++ src/main/java/com/gameshop/dao/ProductDAOImpl.java
@@ -15,29 +15,23 @@ public class ProductDAOImpl implements ProductDAO {
 
 	@Autowired
 	private SessionFactory sessionFactory;
+	
 	private Session currentSession;
+	
 	@Override
 	public List<Product> getProducts() {
-
 		currentSession = sessionFactory.getCurrentSession();
-
-		// create a query .. sort by last name
 		Query<Product> query = currentSession.createQuery("from Product", Product.class);
-		// execute query and get result list
 		List<Product> products = query.getResultList();
-
+		
 		return products;
 	}
 
 	@Override
 	public List<Product> getFourLatestProducts() {
-
 		currentSession = sessionFactory.getCurrentSession();
-
-		// create a query .. sort by last name
 		Query<Product> queryForLatestFourProducts = currentSession.createQuery("from Product order by dateAdded", Product.class);
 		queryForLatestFourProducts.setMaxResults(4);
-		// execute query and get result list
 		List<Product> latestFourProducts = queryForLatestFourProducts.getResultList();
 
 		return latestFourProducts;
@@ -45,9 +39,7 @@ public class ProductDAOImpl implements ProductDAO {
 
 	@Override
 	public Product getProductById(int productId) {
-
 		currentSession = sessionFactory.getCurrentSession();
-
 		Product product = currentSession.get(Product.class, productId);
 
 		return product;
@@ -55,12 +47,10 @@ public class ProductDAOImpl implements ProductDAO {
 	
 	@Override
 	public List<Product> getProductsByCategory(String category) {
-
 		currentSession = sessionFactory.getCurrentSession();
 		String hql = "from Product where category=:category";
 		Query<Product> queryForProductsByCategory = currentSession.createQuery(hql, Product.class);
 		queryForProductsByCategory.setParameter("category",category);
-
 		List<Product> productsByCategory = queryForProductsByCategory.getResultList();
 		
 		return productsByCategory;
@@ -68,19 +58,12 @@ public class ProductDAOImpl implements ProductDAO {
 	
 	@Override
 	public List<Product> searchProductsByName(String productName) {
-		
 		currentSession = sessionFactory.getCurrentSession();
 		String hql = "from Product where productName like :productName ";
 		Query<Product> queryForProductsSearchedByProductName = currentSession.createQuery(hql, Product.class);
 		queryForProductsSearchedByProductName.setParameter("productName", productName + "%");
-
 		List<Product> productsByCategory = queryForProductsSearchedByProductName.getResultList();
 
 		return productsByCategory;
-		
-		
-		
 	}
-
-
 }
